97e1d5b45d2aecb312663963cb4574e7224fded3
Amelia <wattenberger@gmail.com>
2021-02-21 14:09:57 -0500

    fill out modal

--- src/components/Network/NetworkModal.jsx
+++ src/components/Network/NetworkModal.jsx
@@ -18,31 +18,13 @@ const NetworkModal = ({ info = {}, onClose }) => {
 
           <div className="NetworkModal__name">
             {info["label"]}
-            <div className="NetworkModal__year">{info["year"]}</div>
+            {/* <div className="NetworkModal__year">{info["year"]}</div> */}
           </div>
 
           <div className="NetworkModal__status">
             {!!info["Status"] && <StatusPill status={info["Status"][0]} />}
           </div>
 
-          {info["Amount"] && (
-            <div className="NetworkModal__info">
-              ${format(",.0f")(info["Amount"])}
-            </div>
-          )}
-          {info["mainContributionArea"] && (
-            <div className="NetworkModal__info">
-              {info["mainContributionArea"].join(" & ")}
-            </div>
-          )}
-          {info["Entity Type"] && (
-            <div className="NetworkModal__info">
-              {info["Entity Type"].join(", ")}
-            </div>
-          )}
-          {info["Focus"] && (
-            <div className="NetworkModal__info">{info["Focus"]}</div>
-          )}
           {info["Link to More Info on Entity (bio or overview page)"] && (
             <a
               href={info["Link to More Info on Entity (bio or overview page)"]}
@@ -73,6 +55,62 @@ const NetworkModal = ({ info = {}, onClose }) => {
               Latest news
             </a>
           )}
+
+          {info["Amount"] && (
+            <div className="NetworkModal__info">
+              <div className="NetworkModal__label">Amount:</div>$
+              {format(",.0f")(info["Amount"])}
+            </div>
+          )}
+          {[
+            "Focus",
+            "Date",
+            "Topical Contribution Area",
+            "Rate-Limiting Feature",
+            "Entity Type",
+            "Year founded / Born",
+            "Funded by",
+            "Enacted/Undertaken By",
+            "Number of People Involved (to 3 sig digits)",
+            "Directly Associated Orgs (e.g., employment/parent org):",
+            "Partners With",
+            "Enacting/Undertaking XYZ Interventions",
+            "Funding XYZ Interventions",
+            "Made XYZ Investment(s)",
+            "Received XYZ Investment(s)",
+            // "Amount",
+            "Source",
+            "Recipient",
+            "Directly Associated Orgs (e.g., employment/parent org)",
+          ].map(
+            (key) =>
+              info[key] && (
+                <div className="NetworkModal__info">
+                  <div className="NetworkModal__label">
+                    {key.split(" (")[0]}:
+                  </div>
+                  {Array.isArray(info[key])
+                    ? info[key].map((d) => <div>{d}</div>)
+                    : info[key]}
+                </div>
+              )
+          )}
+          {/* {info["mainContributionArea"] && (
+            <div className="NetworkModal__info">
+              {info["mainContributionArea"].join(" & ")}
+            </div>
+          )} */}
+          {info["Intervention(s) being addressed:"] &&
+            !!info["Intervention(s) being addressed:"].filter((d) => d)
+              .length && (
+              <div className="NetworkModal__info">
+                <div className="NetworkModal__label">
+                  Interventions being addressed:
+                </div>
+
+                {info["Intervention(s) being addressed:"].join(", ")}
+              </div>
+            )}
         </div>
       </div>
     </>
