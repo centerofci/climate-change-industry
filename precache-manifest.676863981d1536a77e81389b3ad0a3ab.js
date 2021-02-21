475e18afc74628ff6a325f4f59cbe02a3652342a
Amelia <wattenberger@gmail.com>
2021-02-21 12:15:11 -0500

    Updates

---
+++ precache-manifest.676863981d1536a77e81389b3ad0a3ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6305c05963570819f3b6",
+    "url": "/climate-change-industry/static/css/main.aa63efa0.chunk.css"
+  },
+  {
+    "revision": "6305c05963570819f3b6",
+    "url": "/climate-change-industry/static/js/main.985caab4.chunk.js"
+  },
+  {
+    "revision": "df294fdfd50e19dc2956",
+    "url": "/climate-change-industry/static/js/runtime~main.9d9ef6f6.js"
+  },
+  {
+    "revision": "71542aa5311967e4deed",
+    "url": "/climate-change-industry/static/js/2.1001302d.chunk.js"
+  },
+  {
+    "revision": "217c765518b8f2f2d5d4b0b040946d51",
+    "url": "/climate-change-industry/index.html"
+  }
+];
