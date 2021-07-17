f2e1e43776b829727cc6c8fccdb907ec10bbcce4
Wattenberger <Wattenberger@users.noreply.github.com>
2021-07-17 17:02:27 +0000

    deploy: 1d33a7ba0ab99ec31da6a24de2fd2441f4aec152

---
+++ precache-manifest.5f4342daaa619931bd36d8d15f3755b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e9dc287767e18430110",
+    "url": "/climate-change-industry/static/css/main.c8ac5f6b.chunk.css"
+  },
+  {
+    "revision": "0e9dc287767e18430110",
+    "url": "/climate-change-industry/static/js/main.ce273f7f.chunk.js"
+  },
+  {
+    "revision": "df294fdfd50e19dc2956",
+    "url": "/climate-change-industry/static/js/runtime~main.9d9ef6f6.js"
+  },
+  {
+    "revision": "1b4c4b2b11ce964b9951",
+    "url": "/climate-change-industry/static/js/2.58d01f95.chunk.js"
+  },
+  {
+    "revision": "237b420ec59463d97996281a4d5e2613",
+    "url": "/climate-change-industry/static/media/map.237b420e.png"
+  },
+  {
+    "revision": "1b8dfeaf9d85c224da1a0d4765fe6bcd",
+    "url": "/climate-change-industry/index.html"
+  }
+];
